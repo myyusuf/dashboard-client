f91587679a02d71107fa0348971497c11b0619ce
myyusuf <myyusuf1911@gmail.com>
2016-03-29 01:37:41 +0700

    Finishing PropertyPanel

--- src/components/summary/PropertyPanel.jsx
+++ src/components/summary/PropertyPanel.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var Reflux = require('reflux');
 var ReactBootstrap = require('react-bootstrap');
 var Row = ReactBootstrap.Row;
 var Col = ReactBootstrap.Col;
@@ -6,21 +7,61 @@ var Col = ReactBootstrap.Col;
 var NumberStats = require('../NumberStats');
 var PropertyTable = require('./PropertyTable');
 
+var Formatter = require('../../utils/Formatter');
+var Actions = require('../../stores/Actions');
+var PropertyStore = require('../../stores/PropertyStore');
+
 module.exports = PropertyPanel = React.createClass({
   statics: {},
+  mixins: [Reflux.listenTo(PropertyStore, 'onChange')],
+  getInitialState: function() {
+    return {
+      propertyUnitList: []
+    }
+  },
   getDefaultProps: function() {
     return {id: '', title: 'Title', caption: 'Caption', position: 0}
   },
+  onChange: function(event, result) {
+
+    var _eventType = result.eventType;
+
+    if(_eventType === 'success'){
+      this.setState({propertyUnitList: result.data}, function(){
+        App.unblockUI('#' + this.props.id);
+      }.bind(this));
+    }else if(_eventType === 'error'){
+      this.setState({propertyUnitList: []}, function(){
+        App.unblockUI('#' + this.props.id);
+      }.bind(this));
+    }else if(_eventType === 'startRequest'){
+      App.blockUI({
+          target: '#' + this.props.id,
+          boxed: true
+      });
+    }
+
+  },
   render: function() {
+
+    var _totalUnitCount = 0;
+    var _totalUnitSoldCount = 0;
+    var _propertyUnitList = this.state.propertyUnitList;
+
+    for(var _i=0; _i<_propertyUnitList.length; _i++){
+      var _propertyUnit = _propertyUnitList[_i];
+      _totalUnitCount += _propertyUnit.unitCountThisMonth;
+      _totalUnitSoldCount += _propertyUnit.unitSoldCountThisMonth;
+    }
 
     return (
       <div>
-        <Row className="number-stats">
+        <Row id={this.props.id} className="number-stats">
           <Col md={6} sm={6} xs={6} className="margin-bottom-20">
-            <NumberStats title="Total Unit" caption="0"/>
+            <NumberStats title="Total Unit" caption={Formatter.formatNumber(_totalUnitCount, 0)}/>
           </Col>
           <Col md={6} sm={6} xs={6} className="margin-bottom-20">
-            <NumberStats title="Unit Terjual" caption="0" position={NumberStats.positionRight}/>
+            <NumberStats title="Unit Terjual" caption={Formatter.formatNumber(_totalUnitSoldCount, 0)} position={NumberStats.positionRight}/>
           </Col>
         </Row>
         <PropertyTable id="wgPropertyTable"/>
