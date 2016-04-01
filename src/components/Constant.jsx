b5b679e8c6744ee9e847cd14f7d53fa489220a33
myyusuf <myyusuf1911@gmail.com>
2016-04-02 00:58:36 +0700

    Fix pdf viewer

--- src/components/Constant.jsx
+++ src/components/Constant.jsx
@@ -1,3 +1,6 @@
 exports.YEAR_LIST = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
 
-exports.VIEW_PDF_PATH = '/sample/showpdf/'
+// exports.VIEW_PDF_PATH = '/sample/showpdf/'
+
+//Production
+exports.VIEW_PDF_PATH = '/showpdf/'
